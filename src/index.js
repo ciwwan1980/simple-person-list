1e335b6a7e1259a6a8d23639a07b4aa5db93d48d
Ajil Abdalqader <ajilqado@gmail.com>
2020-07-19 01:31:49 +0200

    destructuring-props-and-img-as-varibale

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Person = () => {
-   const url = `https://randomuser.me/api/portraits/thumb/men/61.jpg`;
+const Person = ({ img, name, job, children }) => {
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
   return (
     <article >
    <img  src={url} alt=""/>
-    <h4>name</h4>
-    <h4>job</h4>
+    <h4>{name}</h4>
+    <h4>{job}</h4>
     </article>
   );
 };
@@ -16,7 +16,9 @@ const PersonList = () => {
 
   return (
     <section >
-  <Person img="56" name="david" job="the boss" />
+    <Person img="56" name="david" job="designer" />
+    <Person img="44" name="schnider" job="developer" />
+    <Person img="11" name="bob" job="artist" />
     </section>
   );
 };
