0675557744053df624548d0ceff4abbc46938d67
Ajil Abdalqader <ajilqado@gmail.com>
2020-07-19 01:41:13 +0200

    childrenProps-and-styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 const Person = ({ img, name, job, children }) => {
   const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
   return (
-    <article >
+    <article className="person">
    <img  src={url} alt=""/>
     <h4>{name}</h4>
     <h4>{job}</h4>
+    {children}
     </article>
   );
 };
@@ -15,10 +17,16 @@ const Person = ({ img, name, job, children }) => {
 const PersonList = () => {
 
   return (
-    <section >
+    <section  className="person-list" >
     <Person img="56" name="david" job="designer" />
-    <Person img="44" name="schnider" job="developer" />
+    <Person img="22" name="bob" job="designer">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores,
+          tempora!
+        </p>
+      </Person>
     <Person img="11" name="bob" job="artist" />
+
     </section>
   );
 };
